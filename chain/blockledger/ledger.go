3c13ceb9b50856694e6eeb220d1757fe9e4be8a4
dyfromnil <dyfromnil@gmail.com>
2021-01-18 22:11:32 +0800

    ledger interface

---
+++ chain/blockledger/ledger.go
@@ -0,0 +1,22 @@
+package blockledger
+
+import (
+	cb "github.com/dyfromnil/pdag/proto-go/common"
+)
+
+//Reader for
+type Reader interface {
+	TipsBlock() []*cb.Block
+}
+
+// Writer allows the caller to modify the ledger
+type Writer interface {
+	// Append a new block to the ledger
+	Append(block *cb.Block) error
+}
+
+// ReadWriter encapsulates the read/write functions of the ledger
+type ReadWriter interface {
+	Reader
+	Writer
+}
