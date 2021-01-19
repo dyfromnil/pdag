e1909b30ac18e680e742283bf56a9e5ee9598487
dyfromnil <dyfromnil@gmail.com>
2021-01-19 19:25:54 +0800

    tiny changes

--- go.mod
+++ go.mod
@@ -3,6 +3,20 @@ module github.com/dyfromnil/pdag
 go 1.15
 
 require (
+	github.com/Shopify/sarama v1.27.2 // indirect
+	github.com/davecgh/go-spew v1.1.1
+	github.com/deckarep/golang-set v1.7.1
 	github.com/golang/protobuf v1.4.3
+	github.com/hashicorp/go-version v1.2.1 // indirect
+	github.com/hyperledger/fabric v2.1.1+incompatible
+	github.com/hyperledger/fabric-amcl v0.0.0-20200424173818-327c9e2cf77a // indirect
+	github.com/hyperledger/fabric-protos-go v0.0.0-20201028172056-a3136dde2354
+	github.com/mitchellh/mapstructure v1.4.1 // indirect
+	github.com/pkg/errors v0.9.1
+	github.com/spf13/viper v1.7.1 // indirect
+	github.com/sykesm/zap-logfmt v0.0.4 // indirect
+	github.com/syndtr/goleveldb v1.0.0 // indirect
+	go.uber.org/zap v1.16.0 // indirect
 	google.golang.org/protobuf v1.25.0
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- test/test.go
+++ test/test.go
@@ -1,14 +1,41 @@
 package main
 
-import "fmt"
-import "time"
+import (
+	"fmt"
+	"github.com/deckarep/golang-set"
+)
 
 func main() {
-	for i := 0; i < 10; i++ {
-		j := i
-		go func() {
-			fmt.Println(j)
-		}()
+
+	var aka mapset.Set
+	aka = mapset.NewSet()
+	aka.Add(34)
+	aka.Add(34)
+	aka.Add(34)
+	aka.Add(64)
+	it := aka.Iterator()
+	var intlist []int
+	for elem := range it.C {
+		intlist = append(intlist, elem.(int))
 	}
-	time.Sleep(time.Duration(time.Second))
+	it.Stop()
+	for _, e := range intlist {
+		fmt.Println(e)
+	}
+
+	requiredClasses := mapset.NewSet()
+	requiredClasses.Add("Cooking")
+	requiredClasses.Add("English")
+	requiredClasses.Add("Math")
+	requiredClasses.Add("Biology")
+	requiredClasses.Add(574)
+	requiredClasses.Remove("Cooking")
+	requiredClasses.Remove("aaaaa")
+
+	it = requiredClasses.Iterator()
+	for elem := range it.C {
+		fmt.Println(elem)
+	}
+	it.Stop()
+
 }
