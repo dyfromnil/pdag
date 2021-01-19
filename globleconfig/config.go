a87433e005a79f807f54a8a339f368ff5f1ab474
dyfromnil <dyfromnil@gmail.com>
2021-01-19 19:26:46 +0800

    add dir&filesize

--- globleconfig/config.go
+++ globleconfig/config.go
@@ -7,4 +7,11 @@ const (
 	MaxMessageCount = 20
 	//BatchTimeOut is
 	BatchTimeOut = 2
+
+	//BlockStorageDir is
+	BlockStorageDir = "./"
+	// ChainsDir is the name of the directory containing the channel ledgers.
+	ChainsDir = "chainStore"
+	//DefaultMaxBlockfileSize is
+	DefaultMaxBlockfileSize = 64 * 1024 * 1024 // bytes
 )
