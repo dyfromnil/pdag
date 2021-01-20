ccc35cea33d043e1d8ead745c69ab4a80b6969cf
dyfromnil <dyfromnil@gmail.com>
2021-01-20 13:26:17 +0800

    start server

--- node/server.go
+++ node/server.go
@@ -2,7 +2,13 @@ package node
 
 import (
 	"fmt"
+	"github.com/dyfromnil/pdag/chain"
+	"github.com/dyfromnil/pdag/chain/blkstorage"
+	"github.com/dyfromnil/pdag/chain/blockledger/fileledger"
+	"github.com/dyfromnil/pdag/client"
+	"github.com/dyfromnil/pdag/consensus/solo"
 	cb "github.com/dyfromnil/pdag/proto-go/common"
+	"time"
 )
 
 //Main for server starting
@@ -10,19 +16,36 @@ func Main() {
 	fmt.Println("start...")
 
 	envCh := make(chan cb.Envelope, 200)
-	for i := 0; i < 3; i++ {
-		i0 := i
-		go func() {
-			j := 0
-			for {
-				envCh <- cb.Envelope{
-					User:      uint32(i0),
-					Payload:   []byte(fmt.Sprintf("This is User %d, message %d", i0, j)),
-					Signature: []byte(fmt.Sprintf("User %d", i0)),
-				}
-				j++
-			}
-		}()
-	}
 
+	clt := client.NewClient(3)
+	clt.SendEnv(envCh)
+
+	conf := blkstorage.NewConf("", 0)
+	blkstore, _ := blkstorage.NewBlockStore(conf)
+	ledger := fileledger.NewFileLedger(blkstore)
+
+	chainSupport := chain.NewSupport(ledger)
+
+	soloConsensus := solo.New()
+	soloChain := soloConsensus.HandleChain(chainSupport)
+
+	var timer <-chan time.Time
+	timer = time.After(time.Duration(time.Second * 10))
+
+	soloChain.Start()
+
+	fmt.Println("begining receive....")
+	var i int = 0
+	for {
+		i++
+		select {
+		case env := <-envCh:
+			err := soloChain.Order(&env)
+			if err != nil {
+				panic("order panic!")
+			}
+		case <-timer:
+			break
+		}
+	}
 }
