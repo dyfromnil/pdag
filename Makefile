375b21d72a31ee52bc806fa532c0af49e9dec4b6
dyfromnil <dyfromnil@gmail.com>
2021-01-31 16:16:22 +0800

    split genKeys from server start

--- Makefile
+++ Makefile
@@ -6,14 +6,14 @@ exper:
 	sudo rm -rf experiment
 	go build -o experiment/client/main cmd/client/main.go
 	go build -o experiment/server0/main cmd/server/main.go
+	go build -o experiment/genKeys cmd/server/main.go
+
+	cd experiment/ && ./genKeys
+	cp -r experiment/Keys experiment/server0/
+	
 	cp -r experiment/server0/ experiment/server1/
 	cp -r experiment/server0/ experiment/server2/
 	cp -r experiment/server0/ experiment/server3/
 
 client:
 	go build -o experiment/client/main cmd/client/main.go
-
-dispatchMsp:
-	sudo cp -r experiment/server0/Keys experiment/server1/Keys
-	sudo cp -r experiment/server0/Keys experiment/server2/Keys
-	sudo cp -r experiment/server0/Keys experiment/server3/Keys
---
+++ cmd/genKeys/main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/dyfromnil/pdag/globleconfig"
+)
+
+func main() {
+	GenRsaKeys()
+}
+
+// GenRsaKeys for
+func GenRsaKeys() {
+	if !isExist("./Keys") {
+		log.Println("检测到还未生成公私钥目录，正在生成公私钥 ...")
+		err := os.Mkdir("Keys", 0666)
+		if err != nil {
+			log.Panic()
+		}
+
+		n := len(globleconfig.NodeTable)
+
+		for i := 0; i < n; i++ {
+			if !isExist("./Keys/N" + strconv.Itoa(i)) {
+				err := os.Mkdir("./Keys/N"+strconv.Itoa(i), 0666)
+				if err != nil {
+					log.Panic()
+				}
+			}
+			priv, pub := GetKeyPair()
+			privFileName := "Keys/N" + strconv.Itoa(i) + "/N" + strconv.Itoa(i) + "_RSA_PIV"
+			file, err := os.OpenFile(privFileName, os.O_RDWR|os.O_CREATE, 0666)
+			if err != nil {
+				log.Panic(err)
+			}
+			defer file.Close()
+			file.Write(priv)
+
+			pubFileName := "Keys/N" + strconv.Itoa(i) + "/N" + strconv.Itoa(i) + "_RSA_PUB"
+			file2, err := os.OpenFile(pubFileName, os.O_RDWR|os.O_CREATE, 0666)
+			if err != nil {
+				log.Panic(err)
+			}
+			defer file2.Close()
+			file2.Write(pub)
+		}
+		log.Println("已为节点们生成RSA公私钥")
+	}
+}
+
+//GetKeyPair for
+func GetKeyPair() (prvkey, pubkey []byte) {
+	// 生成私钥文件
+	privateKey, err := rsa.GenerateKey(rand.Reader, 1024)
+	if err != nil {
+		panic(err)
+	}
+	derStream := x509.MarshalPKCS1PrivateKey(privateKey)
+	block := &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: derStream,
+	}
+	prvkey = pem.EncodeToMemory(block)
+	publicKey := &privateKey.PublicKey
+	derPkix, err := x509.MarshalPKIXPublicKey(publicKey)
+	if err != nil {
+		panic(err)
+	}
+	block = &pem.Block{
+		Type:  "PUBLIC KEY",
+		Bytes: derPkix,
+	}
+	pubkey = pem.EncodeToMemory(block)
+	return
+}
+
+func isExist(path string) bool {
+	_, err := os.Stat(path)
+	if err != nil {
+		if os.IsExist(err) {
+			return true
+		}
+		if os.IsNotExist(err) {
+			return false
+		}
+		log.Println(err)
+		return false
+	}
+	return true
+}
