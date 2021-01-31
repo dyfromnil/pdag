7ecea6aa52d1f670be57eda7ad382822dc481676
dyfromnil <dyfromnil@gmail.com>
2021-01-31 21:52:07 +0800

    fix problem(pbft:eg. receive commit msg before preprepareMsg

--- consensus/pbft/consensus.go
+++ consensus/pbft/consensus.go
@@ -260,7 +260,7 @@ func (pb *Server) HandlePrePrepare(ctx context.Context, pp *cb.PrePrepareMsg) (*
 	primaryNodePubKey := pb.ch.support.GetIdendity().GetPubKey(globleconfig.LeaderNodeID)
 	digestByte, _ := hex.DecodeString(pp.Digest)
 	if digest := blkstorage.BlockHeaderDigest(pp.Block.Header); digest != pp.Digest {
-		log.Printf("信息摘要对不上，拒绝进行prepare广播")
+		log.Fatalln("信息摘要对不上，拒绝进行prepare广播")
 		return &cb.Response{ResCode: false}, errors.New("HandlePrePrepare digest error")
 	} else if !pb.ch.support.GetIdendity().RsaVerySignWithSha256(digestByte, pp.Sign, primaryNodePubKey) {
 		log.Printf("主节点签名验证失败！,拒绝进行prepare广播")
@@ -270,6 +270,8 @@ func (pb *Server) HandlePrePrepare(ctx context.Context, pp *cb.PrePrepareMsg) (*
 		log.Printf("已将消息存入临时节点池")
 		// pb.ch.lock.Lock()
 		pb.ch.blockPool[pp.Digest] = pp.Block
+		log.Println("Pool add digest:", digest)
+
 		// pb.ch.lock.Unlock()
 		//节点使用私钥对其签名
 		sign := pb.ch.support.GetIdendity().RsaSignWithSha256(digestByte, pb.ch.support.GetIdendity().GetSelfPivKey())
@@ -284,6 +286,37 @@ func (pb *Server) HandlePrePrepare(ctx context.Context, pp *cb.PrePrepareMsg) (*
 		log.Printf("正在进行Prepare广播 ...")
 		pb.ch.broadcastPrepare(pre)
 		log.Printf("Prepare广播完成")
+
+		// 接收到Preprepare后发现早已收到足够多后一阶段prepare消息，^_^!节点间延迟太高啦！ @_@几个阶段杂揉了，有时间再改!_!
+		if pb.canPrepared(digest) {
+			log.Println("本节点已收到至少2f个节点(包括本地节点)发来的Prepare信息 ...")
+			//节点使用私钥对其签名
+			sign := pb.ch.support.GetIdendity().RsaSignWithSha256(digestByte, pb.ch.support.GetIdendity().GetSelfPivKey())
+			c := &cb.CommitMsg{
+				Digest: digest,
+				NodeID: pb.ch.support.GetIdendity().GetNodeID(),
+				Sign:   sign,
+			}
+
+			//进行提交信息的广播
+			log.Println("正在进行commit广播", digest)
+			pb.ch.broadcastCommit(c)
+			pb.ch.isCommitBordcast[digest] = true
+			log.Printf("commit广播完成")
+
+			if pb.canCommit(digest) {
+				log.Printf("本节点已收到至少2f + 1 个节点(包括本地节点)发来的Commit信息 ...")
+				//将消息信息，提交到本地消息池中！
+				pb.ch.support.Append(pb.ch.blockPool[c.Digest], pb.ch.tipsList)
+				log.Println("Delete digest from pool: ", c.Digest)
+				info := pb.ch.support.GetIdendity().GetNodeID() + "节点已将当前block存入本地账本中"
+				log.Printf(info)
+				log.Printf("正在reply客户端 ...")
+				tcpDial([]byte(info), globleconfig.ClientAddr)
+				pb.ch.isReply[c.Digest] = true
+				log.Printf("reply完毕")
+			}
+		}
 	}
 	return &cb.Response{ResCode: true}, nil
 }
@@ -300,32 +333,20 @@ func (pb *Server) HandlePrepare(ctx context.Context, p *cb.PrepareMsg) (*cb.Resp
 	_, ok := pb.ch.blockPool[p.Digest]
 	// pb.ch.lock.Unlock()
 	if !ok {
-		log.Printf("当前临时消息池无此摘要，拒绝执行commit广播")
-		return &cb.Response{ResCode: false}, errors.New("HandlePrepare digest error")
+		log.Println("当前临时消息池无此摘要，等待Preprepare消息&暂存当前Prepare消息至prepare池")
+		pb.ch.setPrePareConfirmMap(p.Digest, p.NodeID, true)
+		return &cb.Response{ResCode: false}, nil
 	} else if !pb.ch.support.GetIdendity().RsaVerySignWithSha256(digestByte, p.Sign, MessageNodePubKey) {
 		log.Printf("节点签名验证失败！,拒绝执行commit广播")
 		return &cb.Response{ResCode: false}, errors.New("HandlePrepare VerySign error")
 	} else {
 		// pb.ch.lock.Lock()
 		pb.ch.setPrePareConfirmMap(p.Digest, p.NodeID, true)
-		count := 0
-		for range pb.ch.prePareConfirmCount[p.Digest] {
-			count++
-		}
-		// pb.ch.lock.Unlock()
-		//因为主节点不会发送Prepare，所以不包含自己
-		specifiedCount := 0
-		nodeCount := len(pb.ch.support.GetIdendity().GetClusterAddrs())
-		if pb.ch.support.GetIdendity().GetNodeID() == globleconfig.LeaderNodeID {
-			specifiedCount = nodeCount / 3 * 2
-		} else {
-			specifiedCount = (nodeCount / 3 * 2) - 1
-		}
 		//如果节点至少收到了2f个prepare的消息（包括自己）,并且没有进行过commit广播，则进行commit广播
 		// pb.ch.lock.Lock()
 		//获取消息源节点的公钥，用于数字签名验证
-		if count >= specifiedCount && !pb.ch.isCommitBordcast[p.Digest] {
-			log.Printf("本节点已收到至少2f个节点(包括本地节点)发来的Prepare信息 ...")
+		if pb.canPrepared(p.Digest) {
+			log.Println("本节点已收到至少2f个节点(包括本地节点)发来的Prepare信息 ...", p.Digest)
 			//节点使用私钥对其签名
 			sign := pb.ch.support.GetIdendity().RsaSignWithSha256(digestByte, pb.ch.support.GetIdendity().GetSelfPivKey())
 			c := &cb.CommitMsg{
@@ -335,14 +356,42 @@ func (pb *Server) HandlePrepare(ctx context.Context, p *cb.PrepareMsg) (*cb.Resp
 			}
 
 			//进行提交信息的广播
-			log.Printf("正在进行commit广播")
+			log.Println("正在进行commit广播", p.Digest)
 			pb.ch.broadcastCommit(c)
 			pb.ch.isCommitBordcast[p.Digest] = true
 			log.Printf("commit广播完成")
+
+			// 接收到prepare后发现早已收到足够多后一阶段commit消息，^_^!节点间延迟太高啦！ @_@几个阶段杂揉了，有时间再改!_!
+			if pb.canCommit(p.Digest) {
+				log.Printf("本节点已收到至少2f + 1 个节点(包括本地节点)发来的Commit信息 ...")
+				//将消息信息，提交到本地消息池中！
+				pb.ch.support.Append(pb.ch.blockPool[p.Digest], pb.ch.tipsList)
+				log.Println("Delete digest from pool: ", p.Digest)
+				info := pb.ch.support.GetIdendity().GetNodeID() + "节点已将当前block存入本地账本中"
+				log.Printf(info)
+				log.Printf("正在reply客户端 ...")
+				tcpDial([]byte(info), globleconfig.ClientAddr)
+				pb.ch.isReply[p.Digest] = true
+				log.Printf("reply完毕")
+			}
 		}
 		// pb.ch.lock.Unlock()
 	}
 	return &cb.Response{ResCode: true}, nil
+}
+
+func (pb *Server) canPrepared(digest string) bool {
+	count := len(pb.ch.prePareConfirmCount[digest])
+	// pb.ch.lock.Unlock()
+	//因为主节点不会发送Prepare，所以不包含自己
+	specifiedCount := 0
+	nodeCount := len(pb.ch.support.GetIdendity().GetClusterAddrs())
+	if pb.ch.support.GetIdendity().GetNodeID() == globleconfig.LeaderNodeID {
+		specifiedCount = nodeCount / 3 * 2
+	} else {
+		specifiedCount = (nodeCount / 3 * 2) - 1
+	}
+	return count >= specifiedCount && !pb.ch.isCommitBordcast[digest]
 }
 
 //HandleCommit for
@@ -357,40 +406,38 @@ func (pb *Server) HandleCommit(ctx context.Context, c *cb.CommitMsg) (*cb.Respon
 	_, ok := pb.ch.prePareConfirmCount[c.Digest]
 	// pb.ch.lock.Unlock()
 	if !ok {
-		log.Printf("当前prepare池无此摘要，拒绝将信息持久化到本地消息池")
-		return &cb.Response{ResCode: false}, errors.New("HandleCommit digest error")
+		pb.ch.setCommitConfirmMap(c.Digest, c.NodeID, true)
+		log.Println("当前prepare池无此摘要，等待prepare消息&暂存当前commit消息至CommitConfirm池")
+		return &cb.Response{ResCode: false}, nil
 	} else if !pb.ch.support.GetIdendity().RsaVerySignWithSha256(digestByte, c.Sign, MessageNodePubKey) {
 		log.Printf("节点签名验证失败！,拒绝将信息持久化到本地消息池")
 		return &cb.Response{ResCode: false}, errors.New("HandleCommit VerySign error")
 	} else {
 		// pb.ch.lock.Lock()
 		pb.ch.setCommitConfirmMap(c.Digest, c.NodeID, true)
-		count := 0
-		for range pb.ch.commitConfirmCount[c.Digest] {
-			count++
-		}
-		//如果节点至少收到了2f+1个commit消息（包括自己）,并且节点没有回复过,并且已进行过commit广播，则提交信息至本地消息池，并reply成功标志至客户端！
-		nodeCount := len(pb.ch.support.GetIdendity().GetClusterAddrs())
-		if count >= nodeCount/3*2 && !pb.ch.isReply[c.Digest] && pb.ch.isCommitBordcast[c.Digest] {
+
+		if pb.canCommit(c.Digest) {
 			log.Printf("本节点已收到至少2f + 1 个节点(包括本地节点)发来的Commit信息 ...")
 			//将消息信息，提交到本地消息池中！
 			pb.ch.support.Append(pb.ch.blockPool[c.Digest], pb.ch.tipsList)
+			log.Println("Delete digest from pool: ", c.Digest)
 			info := pb.ch.support.GetIdendity().GetNodeID() + "节点已将当前block存入本地账本中"
 			log.Printf(info)
 			log.Printf("正在reply客户端 ...")
 			tcpDial([]byte(info), globleconfig.ClientAddr)
 			pb.ch.isReply[c.Digest] = true
 			log.Printf("reply完毕")
-
-			// delete(pb.ch.blockPool, c.Digest)
-			// delete(pb.ch.prePareConfirmCount, c.Digest)
-			// delete(pb.ch.commitConfirmCount, c.Digest)
-			// delete(pb.ch.isCommitBordcast, c.Digest)
-			// delete(pb.ch.isReply, c.Digest)
 		}
 		// pb.ch.lock.Unlock()
 	}
 	return &cb.Response{ResCode: true}, nil
+}
+
+func (pb *Server) canCommit(digest string) bool {
+	count := len(pb.ch.commitConfirmCount[digest])
+	//如果节点至少收到了2f+1个commit消息（包括自己）,并且节点没有回复过,并且已进行过commit广播，则提交信息至本地消息池，并reply成功标志至客户端！
+	nodeCount := len(pb.ch.support.GetIdendity().GetClusterAddrs())
+	return count >= nodeCount/3*2 && !pb.ch.isReply[digest] && pb.ch.isCommitBordcast[digest]
 }
 
 //使用tcp发送消息
