7f928656137f96f33f7e9ee5f4e095208081881b
dyfromnil <dyfromnil@gmail.com>
2021-02-01 20:13:27 +0800

    rename package name

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,10 +3,10 @@ package main
 import (
 	"log"
 
-	"github.com/dyfromnil/pdag/node"
+	"github.com/dyfromnil/pdag/server"
 )
 
 func main() {
 	log.SetFlags(log.Ldate | log.Lshortfile | log.Ltime)
-	node.Main()
+	server.Main()
 }
---
+++ server/server.go
@@ -0,0 +1,94 @@
+package server
+
+import (
+	"io"
+	"log"
+	"net"
+	"os"
+
+	"google.golang.org/grpc"
+
+	"github.com/dyfromnil/pdag/chain"
+	"github.com/dyfromnil/pdag/chain/blkstorage"
+	"github.com/dyfromnil/pdag/chain/blockledger/fileledger"
+	"github.com/dyfromnil/pdag/consensus"
+	"github.com/dyfromnil/pdag/consensus/pbft"
+	"github.com/dyfromnil/pdag/globleconfig"
+	"github.com/dyfromnil/pdag/msp"
+	cb "github.com/dyfromnil/pdag/proto-go/common"
+)
+
+//Main for server starting
+func Main() {
+	log.Printf("Server starting...")
+
+	// ----------check identity------------
+	var idt *msp.Identity
+
+	if len(os.Args) != 2 {
+		log.Fatal("Input error")
+	}
+	nodeID := os.Args[1]
+	if _, ok := globleconfig.NodeTable[nodeID]; ok {
+		idt = msp.NewIdt(nodeID)
+	} else {
+		log.Fatal("无此节点编号！")
+	}
+
+	//----- config node : ledger and consensus -----
+	conf := blkstorage.NewConf("", 0)
+	blkstore, _ := blkstorage.NewBlockStore(conf)
+	ledger := fileledger.NewFileLedger(blkstore)
+
+	chainSupport := chain.NewSupport(ledger, idt)
+
+	//--------- consensus start ---------
+	pbftServer := pbft.NewServer(chainSupport)
+	pbftServer.Start()
+
+	//------------ Leader Node listen Envelopes from clients to envCh -----------
+	if nodeID == globleconfig.LeaderNodeID {
+		listenEnv := grpc.NewServer()
+		sendEnvelopsService := &SendEnvelopsService{
+			ch: pbftServer.HandleChain(chainSupport),
+		}
+		cb.RegisterSendEnvelopsServer(listenEnv, sendEnvelopsService)
+
+		lis, err := net.Listen("tcp", globleconfig.LeaderListenEnvelopeAddr)
+		if err != nil {
+			log.Fatalf("net.Listen err: %v", err)
+		}
+
+		go listenEnv.Serve(lis)
+		log.Printf("receive pipe started!")
+	}
+
+	select {}
+}
+
+//SendEnvelopsService for
+type SendEnvelopsService struct {
+	ch consensus.Chain
+}
+
+//Request for
+func (s *SendEnvelopsService) Request(stream cb.SendEnvelops_RequestServer) error {
+	for {
+		r, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+
+		// log.Printf("Receive Envelope from %s,sendtime:%d", r.GetClientAddr(), r.GetTimestamp())
+
+		s.ch.Order(r)
+
+		err = stream.Send(&cb.Response{ResCode: true})
+		if err != nil {
+			return err
+		}
+	}
+}
