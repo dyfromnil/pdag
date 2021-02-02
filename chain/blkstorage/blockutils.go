fdedadc6c933a8ade2b9027a24f88af44efd30f4
dyfromnil <dyfromnil@gmail.com>
2021-02-02 19:14:35 +0800

    add some utils func

--- chain/blkstorage/blockutils.go
+++ chain/blkstorage/blockutils.go
@@ -4,11 +4,34 @@ import (
 	"bytes"
 	"crypto/sha256"
 	"encoding/hex"
+	"fmt"
+	"time"
 
 	"github.com/golang/protobuf/proto"
 
 	cb "github.com/dyfromnil/pdag/proto-go/common"
 )
+
+//GensisBlock for
+func GensisBlock() *cb.Block {
+	time := time.Now()
+	gensis := fmt.Sprintln("This is gensis block, created ", time)
+	gensisHash := sha256.Sum256([]byte(gensis))
+	selfHash := [][]byte{}
+	selfHash = append(selfHash, gensisHash[:])
+
+	header := &cb.BlockHeader{
+		PreviousHash: selfHash,
+		DataHash:     gensisHash[:],
+		Round:        0,
+		Timestamp:    time.UnixNano(),
+	}
+	gensisBlock := &cb.Block{
+		Header: header,
+		Data:   &cb.BlockData{Data: selfHash},
+	}
+	return gensisBlock
+}
 
 // NewBlock constructs a block with no data and no metadata.
 func NewBlock(header *cb.BlockHeader, data *cb.BlockData) *cb.Block {
@@ -37,14 +60,15 @@ func BlockHeaderDigest(b *cb.BlockHeader) string {
 func BlocksHeaderHash(bs []*cb.BlockHeader) [][]byte {
 	blocksHeaderHash := [][]byte{}
 	for _, bh := range bs {
-		sum := sha256.Sum256(BlockHeaderBytes(bh))
-		blocksHeaderHash = append(blocksHeaderHash, sum[:])
-	}
-	if len(blocksHeaderHash) == 0 {
-		gensisBytes := sha256.Sum256([]byte("gensis block"))
-		blocksHeaderHash = append(blocksHeaderHash, gensisBytes[:])
+		blocksHeaderHash = append(blocksHeaderHash, BlockHeaderBytes(bh))
 	}
 	return blocksHeaderHash
+}
+
+//BlockHeaderHash for
+func BlockHeaderHash(header *cb.BlockHeader) []byte {
+	sum := sha256.Sum256(BlockHeaderBytes(header))
+	return sum[:]
 }
 
 //BlockDataHash for
